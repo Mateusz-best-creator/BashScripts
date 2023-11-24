bd37c2f75ec5658f78bc0d0efe950b0bd2b84c06
Mateusz Wieczorek <114943990+Mateusz-best-creator@users.noreply.github.com>
2023-11-24 11:17:09 +0100

    Create SmallScript3.sh

---
+++ SmallScript3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+zenity --forms --title="Give informations about searching" \
+--text="Give informations about searching" \
+--separator="," \
+--add-entry="Filename" \
+--add-entry="Catalog" \
+--add-entry="Min size in kb" \
+--add-entry="Max size in kb" \
+--add-entry="Group" \
+--add-entry="File content" \
