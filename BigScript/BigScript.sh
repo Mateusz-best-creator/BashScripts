ada31b512505f5fcd9cc1fdbd9f1cbdcb147e419
Mateusz Wieczorek <szachmat2004@gmail.com>
2023-11-26 10:38:41 +0100

    Cleaning check_if_won function, make code shorter and more readable

--- BigScript/BigScript.sh
+++ BigScript/BigScript.sh
@@ -56,30 +56,20 @@ function AI_place_symbol() {
 }
 
 PLAYER_WON="0"
-function player_won() {
-    if [[ (${BOARD_SYMBOLS[0]} == $USER_SYMBOL && ${BOARD_SYMBOLS[1]} == $USER_SYMBOL && ${BOARD_SYMBOLS[2]} == $USER_SYMBOL) ||
-          (${BOARD_SYMBOLS[3]} == $USER_SYMBOL && ${BOARD_SYMBOLS[4]} == $USER_SYMBOL && ${BOARD_SYMBOLS[5]} == $USER_SYMBOL) || 
-          (${BOARD_SYMBOLS[6]} == $USER_SYMBOL && ${BOARD_SYMBOLS[7]} == $USER_SYMBOL && ${BOARD_SYMBOLS[8]} == $USER_SYMBOL) || 
-          (${BOARD_SYMBOLS[0]} == $USER_SYMBOL && ${BOARD_SYMBOLS[3]} == $USER_SYMBOL && ${BOARD_SYMBOLS[6]} == $USER_SYMBOL) || 
-          (${BOARD_SYMBOLS[1]} == $USER_SYMBOL && ${BOARD_SYMBOLS[4]} == $USER_SYMBOL && ${BOARD_SYMBOLS[7]} == $USER_SYMBOL) ||
-          (${BOARD_SYMBOLS[2]} == $USER_SYMBOL && ${BOARD_SYMBOLS[5]} == $USER_SYMBOL && ${BOARD_SYMBOLS[8]} == $USER_SYMBOL) ||
-          (${BOARD_SYMBOLS[0]} == $USER_SYMBOL && ${BOARD_SYMBOLS[4]} == $USER_SYMBOL && ${BOARD_SYMBOLS[8]} == $USER_SYMBOL) || 
-          (${BOARD_SYMBOLS[2]} == $USER_SYMBOL && ${BOARD_SYMBOLS[4]} == $USER_SYMBOL && ${BOARD_SYMBOLS[6]} == $USER_SYMBOL) ]]; then
-        PLAYER_WON="1"
-    fi
-}
-
 AI_WON="0"
-function AI_won() {
-    if [[ (${BOARD_SYMBOLS[0]} == $AI_SYMBOL && ${BOARD_SYMBOLS[1]} == $AI_SYMBOL && ${BOARD_SYMBOLS[2]} == $AI_SYMBOL) ||
-          (${BOARD_SYMBOLS[3]} == $AI_SYMBOL && ${BOARD_SYMBOLS[4]} == $AI_SYMBOL && ${BOARD_SYMBOLS[5]} == $AI_SYMBOL) || 
-          (${BOARD_SYMBOLS[6]} == $AI_SYMBOL && ${BOARD_SYMBOLS[7]} == $AI_SYMBOL && ${BOARD_SYMBOLS[8]} == $AI_SYMBOL) || 
-          (${BOARD_SYMBOLS[0]} == $AI_SYMBOL && ${BOARD_SYMBOLS[3]} == $AI_SYMBOL && ${BOARD_SYMBOLS[6]} == $AI_SYMBOL) || 
-          (${BOARD_SYMBOLS[1]} == $AI_SYMBOL && ${BOARD_SYMBOLS[4]} == $AI_SYMBOL && ${BOARD_SYMBOLS[7]} == $AI_SYMBOL) ||
-          (${BOARD_SYMBOLS[2]} == $AI_SYMBOL && ${BOARD_SYMBOLS[5]} == $AI_SYMBOL && ${BOARD_SYMBOLS[8]} == $AI_SYMBOL) ||
-          (${BOARD_SYMBOLS[0]} == $AI_SYMBOL && ${BOARD_SYMBOLS[4]} == $AI_SYMBOL && ${BOARD_SYMBOLS[8]} == $AI_SYMBOL) || 
-          (${BOARD_SYMBOLS[2]} == $AI_SYMBOL && ${BOARD_SYMBOLS[4]} == $AI_SYMBOL && ${BOARD_SYMBOLS[6]} == $AI_SYMBOL) ]]; then
-        AI_WON="1"
+function check_if_won() {
+    SYMBOL=$1
+    if [[ (${BOARD_SYMBOLS[0]} == $SYMBOL && ${BOARD_SYMBOLS[1]} == $SYMBOL && ${BOARD_SYMBOLS[2]} == $SYMBOL) ||
+          (${BOARD_SYMBOLS[3]} == $SYMBOL && ${BOARD_SYMBOLS[4]} == $SYMBOL && ${BOARD_SYMBOLS[5]} == $SYMBOL) || 
+          (${BOARD_SYMBOLS[6]} == $SYMBOL && ${BOARD_SYMBOLS[7]} == $SYMBOL && ${BOARD_SYMBOLS[8]} == $SYMBOL) || 
+          (${BOARD_SYMBOLS[0]} == $SYMBOL && ${BOARD_SYMBOLS[3]} == $SYMBOL && ${BOARD_SYMBOLS[6]} == $SYMBOL) || 
+          (${BOARD_SYMBOLS[1]} == $SYMBOL && ${BOARD_SYMBOLS[4]} == $SYMBOL && ${BOARD_SYMBOLS[7]} == $SYMBOL) ||
+          (${BOARD_SYMBOLS[2]} == $SYMBOL && ${BOARD_SYMBOLS[5]} == $SYMBOL && ${BOARD_SYMBOLS[8]} == $SYMBOL) ||
+          (${BOARD_SYMBOLS[0]} == $SYMBOL && ${BOARD_SYMBOLS[4]} == $SYMBOL && ${BOARD_SYMBOLS[8]} == $SYMBOL) || 
+          (${BOARD_SYMBOLS[2]} == $SYMBOL && ${BOARD_SYMBOLS[4]} == $SYMBOL && ${BOARD_SYMBOLS[6]} == $SYMBOL) ]]; then
+        echo "1"
+    else
+        echo "0"
     fi
 }
 
@@ -107,7 +97,6 @@ PAWNS_ON_BOARD=0
 while [[ true ]]; do
 
     draw_board
-    echo $PLAYER_WON
     if [[ INDEX_WHO_START -eq 0 ]]; then
 
         # Check if player won the game
@@ -139,7 +128,7 @@ while [[ true ]]; do
         PAWNS_ON_BOARD=$((PAWNS_ON_BOARD + 1))
 
         draw_board
-        player_won
+        PLAYER_WON=$(check_if_won "X")
         # Check if AI won the game
         if [[ $PLAYER_WON == "1" ]];then
             echo "Player win"
@@ -158,7 +147,7 @@ while [[ true ]]; do
         PAWNS_ON_BOARD=$((PAWNS_ON_BOARD + 1))
 
         draw_board
-        AI_won
+        AI_WON=$(check_if_won "O")
         
         # Check if AI won the game
         if [[ $AI_WON == "1" ]];then
@@ -173,7 +162,7 @@ while [[ true ]]; do
         PAWNS_ON_BOARD=$((PAWNS_ON_BOARD + 1))
     fi
 
-    player_won
-    AI_won
+    PLAYER_WON=$(check_if_won "X")
+    AI_WON=$(check_if_won "O")
 
 done
